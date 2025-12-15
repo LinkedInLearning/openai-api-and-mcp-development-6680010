05c3c613c07f11588a82cc73f5355dbd0ffb69cc
Sandy <sandy@MacBookPro.lan>
2025-12-15 13:57:13 +0100

    01_02_e

--- exercise-files/01/01_01/main.py
+++ exercise-files/01/01_01/main.py
@@ -19,3 +19,26 @@ response = client.responses.create(
 )
 
 print(response.output_text)
+
+
+response = client.images.generate(
+    model="dall-e-3",        
+    prompt=response.output[0].content[0].text,
+    n=1,
+    size="1024x1024",
+    response_format="b64_json",  # <-- REQUIRED if you want base64 data
+)
+
+pprint(response)
+
+img = response.data[0]
+
+
+if img.b64_json:
+    image_bytes = base64.b64decode(img.b64_json)
+    with open("image.png", "wb") as f:
+        f.write(image_bytes)
+
+    console.print(Fore.GREEN + "Image saved as image.png")
+else:
+    console.print(Fore.RED + "No base64 data in response. Try checking response_format or use img.url instead.")
